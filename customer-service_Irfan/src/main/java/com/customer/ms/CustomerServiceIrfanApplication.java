a84040e3e7cde448e41e5aab2b791d7b4c10dab9
IrfanHussain1 <fanijigg@gmail.com>
2020-10-05 16:46:30 -0400

    Added POM ArtifactID

---
+++ customer-service_Irfan/src/main/java/com/customer/ms/CustomerServiceIrfanApplication.java
@@ -0,0 +1,13 @@
+package com.customer.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceIrfanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceIrfanApplication.class, args);
+	}
+
+}
